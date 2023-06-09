44f4d0dc9b60383ce900d9000253a7f211a03bf2
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2023-06-09 15:58:49 -0400

    Add files via upload

---
+++ Segmentation /CRACKTREE260 dataset and UNet/shellscript_CRACKTREE.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#SBATCH --job-name=tamimadnan    # create a short name for your job
+#SBATCH --partition=Andromeda        # partition 
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=2               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=24        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=30gb         # memory per cpu-core (4G is default)
+#SBATCH --time=48:00:00          # maximum time needed (HH:MM:SS)
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=tadnan@uncc.edu
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+## the above is multithreaded job 
+
+module load pytorch
+
+epochs=30
+
+for epoch in "${epochs[@]}"
+do
+      srun --nodes=1 --ntasks=2 --cpus-per-task=$SLURM_CPUS_PER_TASK python UNet_thesis_CRACKTREE.py "$epoch" > output_epoch_${epoch}.txt & # launch each inner loop iteration as a separate task in the background
+done
+
+wait # wait for all the tasks to complete before exiting
